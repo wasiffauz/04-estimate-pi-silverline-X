94f760ce6538cddaffe63c1df7d9a2b7e8d2400a
silverline-X <84576570+silverline-X@users.noreply.github.com>
2021-05-30 00:22:23 +0530

    Update monte_carlo.c

--- monte_carlo.c
+++ monte_carlo.c
@@ -3,18 +3,32 @@
 #include <stdio.h>
 #include <math.h>
 
-float mc_pi(int);
-
 float frandom() {
   long int q = random();
   float ret = (float)q/(float)RAND_MAX;
   return ret;
 }
+float mc_pi(int a)
+	{
+	float x,y,d;int p=0;
+	for(int i=0;i<a;i++)
+		{
+		x=frandom();
+		y= frandom();
+		d=sqrtf(x*x+y*y);
+		
+		if(d>=1)
+			{p++;}
+		}
+	float ret=16*(float)p/(float)a;
+	return ret;
+	}
+
 
 int main(void) {
   float pi0;
   float pi1;
-  
+  //printf("%f",mc_pi(1));
   pi0 = mc_pi(25000);
   pi1 = mc_pi(25000);
   printf("%f %f\n", pi0, pi1);
@@ -28,8 +42,7 @@ int main(void) {
       printf("Two separate estimates %f and %f are too different.\n", pi0, pi1);
       abort();
   }
-
-    
+  
   for (int i=2000; i<5000; i++) {
     pi0 = mc_pi(i);
     if (!(fabs(pi0 - M_PI) < 0.4)) {
