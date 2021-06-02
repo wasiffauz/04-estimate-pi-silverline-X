702863a49f1955d70b04f30c9c363074746efdc9
silverline-X <84576570+silverline-X@users.noreply.github.com>
2021-06-02 17:26:52 +0530

    Update wallis.c

--- wallis.c
+++ wallis.c
@@ -5,18 +5,24 @@
 
 float wallis_pi(int n)
 	{
-	float pro=1.00;
+	double pro=1.00;
+	int counter=0;
 	for(int i=1;i<=n;i++)
-		{//printf("%d\t%i",4*i*i-1,4*i*i/(4*i*i-1));
-		pro*=(float)4*i*i/(float)((4*i*i)-1);//printf("%f\n",pro);
+		{if(n==2999){
+		double q=(double)4*i*i;
+		//printf("%d\t%f\t\t%.10f\n",i,q-1,q/(q-1));
 		}
+		pro*=((double)(4*i*i))/((4*i*i)-1);
+		counter++;
+		//printf("**%d \t%.20f**\t%i\n",i,(float)4*i*i/((4*i*i)-1),4*i*i);
+		}printf("%d   %d  %.20f\n------------------\n",counter,n,pro*2);
 	return 2*pro;
 	}
-
 int main(void) {
   float pi;
   for (int i=0; i<5; i++) {
     pi = wallis_pi(i);
+    //printf("%f",pi);
     if (!(fabs(pi - M_PI) > 0.15)) {
       printf("Estimate with just %d iterations is %f which is too accurate.\n", i, pi);
       abort();
